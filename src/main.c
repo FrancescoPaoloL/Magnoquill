b7722e4a8cd224db62aca3dd44547d4578f2fe1d
FrancescoPaoloL <francescopaololezza@gmail.com>
2025-07-27 04:52:59 +0200

    refactor: simplify directory creation code

--- src/main.c
+++ src/main.c
@@ -6,24 +6,21 @@
 #include "utils.h"
 
 int main() {
-    if (!create_directory("frames", 0755)) {
-        free(g_image);
-        return 1;
-    }
-
-    if (!create_directory("video", 0755)) {
-        free(g_image);
-        return 1;
-    }
-
     g_image = malloc(IMAGE_SIZE);
     if (!g_image) {
         fprintf(stderr, "Failed to allocate image buffer\n");
         return 1;
     }
-    
-    // WIP
 
+    const char *dirs[] = {"frames", "video"};
+    for (int i = 0; i < 2; i++) {
+        if (!create_directory(dirs[i], 0755)) {
+            free(g_image);
+            return 1;
+        }
+    }
+
+    //WIP
     free(g_image);
     return 0;
 }
